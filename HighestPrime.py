81b049ed46188ceeacbee4b3fa92b70dcdae1824
Brown <brownmr1@jhuapl.edu>
2017-10-02 13:50:10 -0400

    feat:add highest-prime

--- HighestPrime.py
+++ HighestPrime.py
@@ -7,14 +7,39 @@ def highestPrime(n):
 
 
 def isPrime(n):
-    count = 0
+    if (n == 1) | (n == 0):
+        return False
     for x in range(1, int(math.sqrt(n)) + 1):
         if (n % x == 0) & (x != 1):
+            return False
+    return True
+
+
+<<<<<<< Updated upstream
+=======
+def primesList(n):
+    primes = (n+1) * [True]
+    count = 0
+    for x in range(n + 1):
+        if primes[x] == False:
+            continue
+        elif isPrime(x):
+            primes[x] = True
             count += 1
-    if count == 0:
-        return True
-    return False
+        else:
+            primes[x] = False
+        y = 2
+        while (x * y) < (n + 1):
+            primes[x * y] = False
+            y += 1
+    done = count * [0]
+    z = 0
+    for x in range(n + 1):
+        if primes[x] == True:
+            done[z] = primes[x]
+            z += 1
+    return done
 
-
+>>>>>>> Stashed changes
 print(highestPrime(600851475143))
 
